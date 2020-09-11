2101813e4ddec628ad9a601d27938b6be129b7c8
Anderson <faro.anderson@gmail.com>
2020-09-11 09:26:15 -0300

    mudança adicionada

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header({ title }) {
   return (
-    <header>
-      <h1> {title}</h1>
-      
-    </header>
+          <header>
+            <h1> {title}</h1>
+            
+          </header>
   );
 }
